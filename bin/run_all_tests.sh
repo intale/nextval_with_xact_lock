d8906620e5380508091c30ac17fd1f7dc7be112c
Ivan Dzyzenko <intale.a@gmail.com>
2025-12-19 12:46:24 +0100

    Implement bin/run_all_tests.sh

---
+++ bin/run_all_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+script_dir=$(dirname $0)
+
+PGVER=16 ./$script_dir/run_tests.sh || exit 0
+PGVER=17 ./$script_dir/run_tests.sh || exit 0
+PGVER=18 ./$script_dir/run_tests.sh || exit 0
