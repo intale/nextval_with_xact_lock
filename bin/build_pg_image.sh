8070243d338cd9201cb22973d72cbc5eb1093e56
Ivan Dzyzenko <intale.a@gmail.com>
2025-12-23 11:16:40 +0100

    Output useful info in bin/build_pg_image.sh and bin/push_pg_image.sh

--- bin/build_pg_image.sh
+++ bin/build_pg_image.sh
@@ -3,4 +3,5 @@
 PGVER="${PGVER:-18}"
 TAG="${TAG:-idzyzenko/postresql-nextval_with_xact_lock:$PGVER}"
 
+echo "Building $TAG image for PostgreSQL v$PGVER"
 docker build . -t "$TAG" --build-arg PGVER="$PGVER"
--- bin/push_pg_image.sh
+++ bin/push_pg_image.sh
@@ -3,4 +3,5 @@
 PGVER="${PGVER:-18}"
 TAG="${TAG:-idzyzenko/postresql-nextval_with_xact_lock:$PGVER}"
 
+echo "Pushing $TAG"
 docker push "$TAG"
